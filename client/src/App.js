af99ddc8ffff82afdc6f7763ae6d650ae0d33fb0
mlawso33-code <marclawson195@gmail.com>
2022-04-05 12:10:17 -0700

    fixed mongodb conversion, able to connect to db and server.

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <h1>
+        Elden Ring Classes
+      </h1>
+    </div>
+  );
+}
+
+export default App;
